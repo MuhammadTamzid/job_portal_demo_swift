6d57b609aa9768a360bf09b1a218a609572dc5e9
dina28 <kazi.sharmin.dina@framgia.com>
2017-04-18 10:18:27 +0600

    add gitIgnor and initial project

---
+++ job_portal_demo_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  job_portal_demo_swift
+//
+//  Created by Kazi Sharmin Dina on 4/18/17.
+//  Copyright © 2017 Kazi Sharmin Dina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
